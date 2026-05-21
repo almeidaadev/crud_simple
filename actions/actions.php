e16d785d834f19fa3b9ec957f5cab4ad672d4421
almeidaadev <unkn0wn01127800@gmail.com>
2026-05-20 23:38:24 -0300

    database created

--- actions/actions.php
+++ actions/actions.php
@@ -1,7 +1,11 @@
 
 <?php
 
+require __DIR__ . "/../Database/Connection.php";
+
 header("Content-Type: application/json");
+
+
 if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])) {
     echo json_encode("Campos vazios");
     exit();
@@ -20,6 +24,21 @@ $password = $_POST["password"];
 
 switch ($action) {
     case "create":
+        // 2. Prepare the SQL statement with named placeholders
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $stmt = $pdo->prepare($sql);
+
+        $data = [
+            ':name' => $name,
+            ':email' => $email,
+            ':password' => $password
+        ];
+
+        if ($stmt->execute($data)) {
+            echo json_encode("New record created successfully. Last inserted ID: " . $pdo->lastInsertId());
+        } else {
+            echo json_encode("Error inserting data.");
+        }
         echo json_encode("Creating");
         break;
 
